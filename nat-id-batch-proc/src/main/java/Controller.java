6acacd9eeab8db9179d646ecb192536f01f0ad3c
MantasSiup <mantas.siupienius@gmail.com>
2022-06-27 01:00:38 +0300

    Updated Validation

---
+++ nat-id-batch-proc/src/main/java/Controller.java
@@ -0,0 +1,33 @@
+import Validation.Person;
+import Validation.ValidateID;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
+
+import java.util.ArrayList;
+
+@Command(
+    name="helloName",
+    description = "It prints hello [name]"
+)
+public class Controller implements Runnable{
+
+
+    public static void main(String[] args){
+      new CommandLine(new Controller()).execute(args);
+
+
+
+    }
+    @Parameters(index = "0", description = "the name to print")
+    private String name;
+    @Option(names = {"-f", "--firstName"}, description = "testing")
+    private boolean allCaps = false;
+    @Override
+    public void run() {
+        ArrayList<Person> people = InOutUtils.readFromTxt("src/main/resources/in.txt");
+
+        System.out.println("Hello " + (allCaps ? name.toUpperCase() : name));
+    }
+}
